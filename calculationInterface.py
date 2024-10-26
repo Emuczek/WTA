935319ec8b4abc98269193c730ab6650ede696dd
unknown <kubamaldrzyk@gmail.com>
2024-10-26 12:05:27 +0200

    Changed comments and variable name

--- calculationInterface.py
+++ calculationInterface.py
@@ -3,7 +3,6 @@ from abc import abstractmethod
 
 class CalculationInterface:
 
-    # should retur a tuple of main_image_path and list of images
     @abstractmethod
-    def calculate(self, image_path):
+    def calculate(self, data_path):
         pass
