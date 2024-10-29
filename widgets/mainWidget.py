5606ad63eefa148e6718553c7eba72fe8beb814d
Jakub Małdrzykowski <kubamaldrzyk@gmail.com>
2024-10-29 23:13:40 +0100

    Added venv gitignore cleaned mainWidget code

--- widgets/mainWidget.py
+++ widgets/mainWidget.py
@@ -14,43 +14,13 @@ from pyvis.network import Network
 import markdown
 
 
-class FilePicker(QWidget):
-    def __init__(self):
-        super().__init__()
-
-        self.setWindowTitle("File Picker")
-
-        layout = QVBoxLayout()
-
-        self.button = QPushButton("Select File")
-        self.button.clicked.connect(self.open_file_dialog)
-        layout.addWidget(self.button)
-
-        self.setLayout(layout)
-
-    def open_file_dialog(self):
-        file_dialog = QFileDialog(self)
-        # Set options for the dialog (optional)
-        file_dialog.setFileMode(QFileDialog.ExistingFile)  # Only allow selecting existing files
-        # file_dialog.setNameFilter("Text files (*.txt);;Images (*.png *.jpg)") # Set file filters
-
-        if file_dialog.exec():  # Show the dialog and check if the user selected a file
-            selected_file = file_dialog.selectedFiles()[0]
-            print(f"Selected file: {selected_file}")
-            # Do something with the selected file path, e.g., open it, process it, etc.
-
-
 class GraphWidget(QWidget):
     def __init__(self):
         super().__init__()
         self.layout = QVBoxLayout()
         self.setLayout(self.layout)
-
-        # Używamy QWebEngineView do wyświetlania HTML
         self.web_view = QWebEngineView()
         self.layout.addWidget(self.web_view)
-
-        # Tworzymy i rysujemy graf
         self.draw_graph()
 
     def draw_graph(self):
@@ -67,92 +37,6 @@ class GraphWidget(QWidget):
         with open("widgets/temp/graph.html", "r") as f:
             html = f.read()
         self.web_view.setHtml(html)
-
-
-class Dialog(QDialog):
-    num_grid_rows = 3
-    num_buttons = 4
-
-    def __init__(self):
-        super().__init__()
-
-        self._horizontal_group_box = None
-        self._small_editor = None
-        self._grid_group_box = None
-        self._exit_action = None
-        self._file_menu = None
-        self._menu_bar = None
-        self._form_group_box = None
-        self.create_menu()
-        self.create_horizontal_group_box()
-        self.create_grid_group_box()
-        self.create_form_group_box()
-
-        big_editor = QTextEdit()
-        big_editor.setPlainText("This widget takes up all the remaining space "
-                                "in the top-level layout.")
-
-        button_box = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
-
-        button_box.accepted.connect(self.accept)
-        button_box.rejected.connect(self.reject)
-
-        main_layout = QVBoxLayout()
-        main_layout.setMenuBar(self._menu_bar)
-        main_layout.addWidget(self._horizontal_group_box)
-        main_layout.addWidget(self._grid_group_box)
-        main_layout.addWidget(self._form_group_box)
-        main_layout.addWidget(big_editor)
-        main_layout.addWidget(button_box)
-        self.setLayout(main_layout)
-
-        self.setWindowTitle("Basic Layouts")
-
-    def create_menu(self):
-        self._menu_bar = QMenuBar()
-
-        self._file_menu = QMenu("&File", self)
-        self._exit_action = self._file_menu.addAction("E&xit")
-        self._menu_bar.addMenu(self._file_menu)
-
-        self._exit_action.triggered.connect(self.accept)
-
-    def create_horizontal_group_box(self):
-        self._horizontal_group_box = QGroupBox("Horizontal layout")
-        layout = QHBoxLayout()
-
-        for i in range(Dialog.num_buttons):
-            button = QPushButton(f"Button {i + 1}")
-            layout.addWidget(button)
-
-        self._horizontal_group_box.setLayout(layout)
-
-    def create_grid_group_box(self):
-        self._grid_group_box = QGroupBox("Grid layout")
-        layout = QGridLayout()
-
-        for i in range(Dialog.num_grid_rows):
-            label = QLabel(f"Line {i + 1}:")
-            line_edit = QLineEdit()
-            layout.addWidget(label, i + 1, 0)
-            layout.addWidget(line_edit, i + 1, 1)
-
-        self._small_editor = QTextEdit()
-        self._small_editor.setPlainText("This widget takes up about two thirds of the grid layout.")
-
-        layout.addWidget(self._small_editor, 0, 2, 4, 1)
-
-        layout.setColumnStretch(1, 10)
-        layout.setColumnStretch(2, 20)
-        self._grid_group_box.setLayout(layout)
-
-    def create_form_group_box(self):
-        self._form_group_box = QGroupBox("Form layout")
-        layout = QFormLayout()
-        layout.addRow(QLabel("Line 1:"), QLineEdit())
-        layout.addRow(QLabel("Line 2, long text:"), QComboBox())
-        layout.addRow(QLabel("Line 3:"), QSpinBox())
-        self._form_group_box.setLayout(layout)
 
 
 class StatusDockWidget(QDockWidget):
@@ -320,6 +204,8 @@ class MainWindow(QMainWindow):
                 markdown_text = f.read()
             html = markdown.markdown(markdown_text)
 
+            # Documentation as dock widget
+
             # dock = QDockWidget("Information", self)  # Dock title is the file path
             # dock.setAllowedAreas(Qt.AllDockWidgetAreas)
             # text_edit = QTextEdit()
@@ -327,6 +213,8 @@ class MainWindow(QMainWindow):
             # text_edit.setHtml(html)
             # dock.setWidget(text_edit)
             # self.addDockWidget(Qt.RightDockWidgetArea, dock)  # Initial position
+
+            # Documentation as pop window
 
             self.markdown_window.setWindowTitle("Informacje")
             layout = QVBoxLayout()  # Add layout to new window
