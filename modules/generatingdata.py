978e9c025e9e04fd2814fe430616d70c992a1652
Jakub Małdrzykowski <kubamaldrzyk@gmail.com>
2025-03-31 22:11:10 +0200

    Sarted new branch for quantum project

--- modules/generatingdata.py
+++ modules/generatingdata.py
@@ -5,42 +5,37 @@ from itertools import count
 import itertools
 
 
-def generate_random_data(t, weaponTypeCount, targetCount):
-    timeDim = t
-    targetWeights = [random.randint(10000, 100000) for _ in range(targetCount)]
+def generate_random_data(weaponTypeCount, targetCount):
+    targetWeights = [random.randint(10, 100) for _ in range(targetCount)]
     weaponsSupply = [random.randint(1, 3) for _ in range(weaponTypeCount)]
-    targetStartingPosition = [random.randint(1000, 1500) for _ in range(targetCount)]  # m
-    targetVelocity = [random.randint(100, 120) for _ in range(targetCount)]  # m/s
-    weaponRange = [random.randint(900, 2000) for _ in range(weaponTypeCount)]  # m
+    position = [random.randint(1000, 1500) for _ in range(targetCount)] # m
+    weaponRange = [random.randint(100, 1000) for _ in range(weaponTypeCount)]  # m
 
     probabilities = []
     for _ in range(weaponTypeCount):
         probabilities.append([round(random.uniform(0, 0.9), 3) for _ in range(targetCount)])
 
     data = {
-        "timeDim": timeDim,
         "weaponTypeCount": weaponTypeCount,
         "targetCount": targetCount,
         "targetWeights": targetWeights,
         "weaponsSupply": weaponsSupply,
         "propabilities": probabilities,
-        "targetStartingPosition": targetStartingPosition,
-        "targetVelocity": targetVelocity,
+        "position": position,
         "weaponRange": weaponRange
     }
+
+    # Zapisywanie danych do pliku JSON
+    with open(f'../data/{weaponTypeCount}x{targetCount}.json', 'w') as json_file:
+        json.dump(data, json_file, indent=2)
+
+    print("Dane zostały wygenerowane i zapisane w pliku random_data.json.")
 
     return data
 
 
-t = 50
-weaponTypeCount = 50
-targetCount = 50
-
 # Generowanie danych
-random_data = generate_random_data(t, weaponTypeCount, targetCount)
 
-# Zapisywanie danych do pliku JSON
-with open(f'../data/testInstance{t}x{weaponTypeCount}x{targetCount}.json', 'w') as json_file:
-    json.dump(random_data, json_file, indent=2)
-
-print("Dane zostały wygenerowane i zapisane w pliku random_data.json.")
+# weaponTypeCount = 50
+# targetCount = 50
+# random_data = generate_random_data(weaponTypeCount, targetCount)
